ac69163e802575ae72a472609c2e4f284a2ad8f2
GuillermoFuentes <guillermo.fuentes01@alumnos.ucn.cl>
2023-11-22 15:27:42 -0300

    Feat: CRUD BACKEND READY

--- ApiNET/backEnd/Src/Data/Controllers/ProductsController.cs
+++ ApiNET/backEnd/Src/Data/Controllers/ProductsController.cs
@@ -15,5 +15,70 @@ namespace backEnd.Src.Data.Controllers
         }
 
         // GET PRODUCT
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_context.Products);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, Product product)
+        {
+            try
+            {
+                var productToUpdate = _context.Products.Find(id);
+
+                if (productToUpdate == null)
+                {
+                    return NotFound($"No se encontró un producto con ID {id}");
+                }
+
+                productToUpdate.Name = product?.Name ?? productToUpdate.Name;
+                productToUpdate.Price = product?.Price ?? productToUpdate.Price;
+                productToUpdate.Description = product?.Description ?? productToUpdate.Description;
+                productToUpdate.Image = product?.Image ?? productToUpdate.Image;
+
+                _context.SaveChanges();
+                return Ok(productToUpdate);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error al actualizar el producto: {ex.Message}");
+            }
+}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            try
+            {
+                var product = _context.Products.Find(id);
+
+                if (product == null)
+                {
+                    return NotFound($"No se encontró un producto con ID {id}");
+                }
+
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error al eliminar el producto: {ex.Message}");
+            }
+        }
+
+
+
+
     }
 }
